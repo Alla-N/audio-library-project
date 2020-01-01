6df1d56726adc87b6ec89356b784bf8a59ae7cae
Alla-N <Allasandra1725@gmail.com>
2020-01-01 12:49:03 +0200

    create footer player and styling

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -4,7 +4,54 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className = 'footer'>
-            Footer
+            <audio src=""></audio>
+            <div className="progress">
+                <div className="text">
+                    <div className="time current">00:57</div>
+                    <div className="time duration">03:26</div>
+                </div>
+                <div className="value"></div>
+            </div>
+            <div className="controls">
+                <div className="controls_buttons">
+                    <div className="btn prev" title="Предыдущая">
+                        <i className="icon"></i>
+                    </div>
+                    <div className="btn play">
+                        <i className="icon"></i>
+                    </div>
+                    <div className="btn next" title="Следующая">
+                        <i className="icon"></i>
+                    </div>
+                    <div className="btn repeat" title="Зациклить воспроизведение">
+                        <i className="icon"></i>
+                    </div>
+                </div>
+                <div className="mp3">
+                    <span className="title">
+                        <span className="artist_name">Artist name</span>
+                        <span className="song_name">Song name</span>
+                    </span>
+                </div>
+                <div className="actions">
+                    <div className="btn volume">
+                        <i className="icon"></i>
+                        <div className="controls_box" ondragstart="return false;" ondrop="return false;">
+                            <div className="b_range">
+                                <div className="slider"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="btn like" title="Мне нравится">
+                        <i className="icon"></i>
+                    </div>
+                    <div className="btn download" title="Скачать">
+                        <a href="./">
+                            <i class="icon"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
